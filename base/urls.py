0ad4e32d798689980f157837a524573ec323ee6c
deepaktho <deepakthorat900@gmail.com>
2023-02-14 00:13:34 +0530

    .

--- base/urls.py
+++ base/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('insert_data/', views.insert_data, name='insert_data'),
-    path('profiles/' views.profiles, name='profiles' ),
+    # path('profiles/', views.profiles, name='profiles' ),
 ]
